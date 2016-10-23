a4a6a878395edf045f90bc0525e7e4732ddf8d7e
Tarek Moubarak <spacedreamer@tareks-macbook-pro.local>
2016-10-23 08:44:30 +0300

    Initial Commit

---
+++ Flash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flash
+//
+//  Created by Tarek Moubarak on 10/23/16.
+//  Copyright © 2016 HelloTree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
